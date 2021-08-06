ff439dff64490355a09294dcfecb0307634133ab
flavio-ss <9090flavio@gmail.com>
2021-08-05 22:14:16 -0300

    CODE EDUCATION - DESAFIO PRATICO - NGINX - NODE - MYSQL

--- app/index.js
+++ app/index.js
@@ -1,11 +1,73 @@
 const express = require('express')
+const mysql = require('mysql')
+const fake = require('faker');
+
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-    res.send('<h1>Full Cycle Rocks!</h1>')
+const criaTablePeople = `
+    create table if not exists people (
+        id int not null auto_increment,
+        name varchar(1000),
+        primary key(id)
+    )
+`
+const selectPeople = `
+    select * from people
+    order by id desc
+    limit 30
+`
+
+function insertPeople(name) {
+    return `insert into people(name) values ('${name}')`
+}
+
+const config = {
+    host: 'db-app',
+    user: 'root',
+    password: 'root',
+    database: 'database-app'
+}
+
+const conn = mysql.createConnection(config)
+conn.query(criaTablePeople)
+conn.end()
+
+app.get('/', (req,res) => {
+
+    let retorno = '';
+
+    const conn = mysql.createConnection(config)
+    conn.query(insertPeople(fake.name.findName()))
+
+    conn.query(selectPeople, function (err, result, fields) {
+        if (err) throw err;
+        if (result) {
+            result.forEach(p => {
+                retorno += 
+                    `<tr>
+                        <td>${p.id}</td>
+                        <td>${p.name}</td>
+                    </tr>`
+            });
+        }
+
+        res.send(
+            `<h1>Full Cycle Rocks!</h1>
+            <table>
+                <tr>
+                    <th>ID</th>
+                    <th>Nome</th>
+                </tr>
+                ${retorno}
+            </table>`
+        );
+        
+    })
+    conn.end()
+    
 })
 
-app.listen(port, () => {
-    console.log(`Aplicação escutando http://localhost:${port}`)
+app.listen(port, ()=> {
+    console.log('Rodando na porta ' + port);
 })
